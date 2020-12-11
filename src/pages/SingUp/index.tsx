6172df5d5b75ea329e001873ed103fcaa026faf2
DaniloSVJ <danilocotatoceu@gmail.com>
2020-12-11 16:51:30 -0300

    first commi

--- src/pages/SingUp/index.tsx
+++ src/pages/SingUp/index.tsx
@@ -1,34 +1,67 @@
-import React from 'react';
-import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import React, { useCallback, useRef } from 'react';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import type { FormHandles } from '@unform/core';
+
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
-
 import Input from '../../components/input';
 import Button from '../../components/button';
 
-const SingUp: React.FC = () => (
-    <Container>
-        <Content>
-            <img src={logoImg} alt="GoBarber" />
-            <form action="">
-                <h1>Faça seu Cadastro</h1>
-                <Input name="name" icon={FiUser} placeholder="Nome" />
-                <Input name="email" icon={FiMail} placeholder="E-mail" />
-                <Input
-                    name="password"
-                    icon={FiLock}
-                    placeholder="Senha"
-                    type="password"
-                />
-                <Button type="submit">Cadastrar</Button>
-            </form>
-            <a href="forgot">
-                <FiArrowLeft />
-                Voltar para logon
-            </a>
-        </Content>
-        <Background />
-    </Container>
-);
+const SignUp: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
 
-export default SingUp;
+    const handleSubmit = useCallback(async (data: object) => {
+        try {
+            const schema = Yup.object().shape({
+                //  const schema = Yup.object().shape({
+
+                name: Yup.string().required('Nome obrigatorio'),
+                //        name: Yup.string().required('Nome obrigatório'),
+
+                email: Yup.string()
+                    .email('Digite um email válido')
+                    // email: Yup.string()
+                    .required('Email obrigatorio'),
+
+                password: Yup.string().min(7, 'No mínimo 7 digitos'),
+            });
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+        } catch (err) {
+            console.log(err);
+            formRef.current?.setErrors({
+                name: 'Nome Obrigatorio',
+            });
+        }
+    }, []);
+    return (
+        <Container>
+            <Background />
+            <Content>
+                <img src={logoImg} alt="GoBarber" />
+                <Form ref={formRef} onSubmit={handleSubmit}>
+                    <h1>Faça seu Cadastro</h1>
+                    <Input name="name" icon={FiUser} placeholder="Nome" />
+                    <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+                    <Input
+                        name="password"
+                        icon={FiLock}
+                        type="password"
+                        placeholder="Senha"
+                    />
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+                <a href="forgot">
+                    <FiArrowLeft />
+                    Voltar para logon
+                </a>
+            </Content>
+        </Container>
+    );
+};
+
+export default SignUp;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import SingIn from '../pages/SingIn/index';
+import SingUp from '../pages/SingUp/index';
 // import Repository from '../pages/Repository/index';
 // Switch vai garantir que apenas uma rota seja visivel
 const Routes: React.FC = () => (
     <Switch>
-        <Route path="/" exact component={SingIn} />
+        <Route path="/" exact component={SingUp} />
     </Switch>
 );
 
